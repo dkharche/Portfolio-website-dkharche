9f7c45d72e73227044e847133a1f450c87ef3a44
DIVIJ KHARCHE <74908218+dkharche@users.noreply.github.com>
2023-07-28 21:40:21 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
+
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
